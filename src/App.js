ed34a34e6a526a066de736056bc266a05045df0d
Haridu <haridu.senadeera@gmail.com>
2018-01-30 12:32:51 -0600

    added bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -4,20 +4,62 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 import Home from './routes/Home';
 import Trips from './routes/Trips';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
       <Router>
         <div>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/trips">Trips</Link></li>
-          </ul>
+          <Navbar color="faded" light expand="md">
+            <NavbarBrand tag={Link} to="/">Travel With Munchkins</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink tag={Link} to="/trips">
+                    Trips
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} to="/tips">
+                    Tips
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} to="/contact">
+                    Contact
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
 
-          <hr/>
 
           <Route exact path="/" component={Home}/>
           <Route path="/trips" component={Trips}/>
