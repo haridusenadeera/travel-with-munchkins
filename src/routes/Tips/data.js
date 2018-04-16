9f5f2babf23ae2d99926a79e2f57d6290f797bc5
Haridu <haridu.senadeera@gmail.com>
2018-04-16 14:54:36 -0500

    added tips page content

---
+++ src/routes/Tips/data.js
@@ -0,0 +1,131 @@
+const data = {
+  topics: [
+    {
+      id: 'transBooking',
+      title: '1. Transportation Bookings',
+      tips: [
+        'We travel on a budget, yet our travels are very comfortable. We usually pick inexpensive flights and good hotels with very good or above rating on Tripadvisor and/or booking.com.',
+        'We often use budget airlines. Skyscanner is our favorite website. We search for return tickets, but sometimes it\'s cheaper to buy one-way tickets so that we have the flexibility of using multiple airlines and/or nearby airports.',
+        'All most all budget airlines charge extra for seat reservation. We never reserve seats when booking air tickets and have never faced any problems. No airline has ever separated our family! We have mostly used Easyjet  to travel within Europe.',
+        'If convenient enough, we travel by trains. This is an economical option too because oftentimes children under 6 years ride free. When traveling by trains, we make sure to reserve seats if the journey is at least 2 hours long so that we all can sit together and each child gets her own seat. However, seat reservation is usually not possible in slow regional trains within Germany and a few other countries in the region. Bahn.com is a good website to get a feeling of available connections.',
+        'Oftentimes, prior to departure to a new city, we do a good research on local transportation (e.g., airport connections and city trains/trams/buses).',
+        'Cruise lines are also a good way to explore Europe.  When we were in Helsinki, we took an overnight cruise to Tallinn in Estonia and back. Also, we took a visa-free cruise to St. Petersburg from Helsinki.',
+      ]
+    },
+    {
+      id: 'hotelBooking',
+      title: '2. Hotel Bookings',
+      tips: [
+        'We reserve hotels months before the trip. Our favorite is booking.com and we have never had any bad experiences with them. We reserve hotels that have free cancellation policies so that we can cancel our reservations if we find  better deals later.',
+        'Before reserving a hotel, we check the hotel policy on children. Most of the European hotels let children under 6 stay free of charge when using existing beds. Some hotels provide extra beds at no additional charges. We love them because that saves us so much money for other things like eating well. When we stay in a hotel more than 3-4 nights we opt for a family room, if economical. Family rooms usually have more space to move around.',
+        'When looking for hotels, we run the search for only 2 adults (no children) and then check the hotel policy on children (on booking.com) to see if the hotel lets them stay free of charge. When booking a double room (or sometimes a triple room) for our family of four, we always add a note regarding the children and sometimes we call the hotel a few days after booking to make sure they are fine with our booking and children. Some double rooms have sofa beds even though they don\'t advertise (The double rooms that we booked for our family in Helsinki, Dubrovnik, and in a few other cities had sofa beds too.) Yes, we always run a search adding children as well to get a better picture of our options.',
+        'We have a wish list for our travels. We often visit touristic cities in the shoulder times so that we can see the city leisurely and get excellent deals on hotels too. Of course, we visit beach cities when the water is warm enough to have some fun in the water.',
+        'We usually pick a hotel close to major attractions. That saves a lot of time and if the kids get too tired we can always come back to the hotel and let them rest up for a little while.  In big cities, we tend to stay close to a train station, usually one of the main stations with many connections to most parts of the city and buy daily or weekly passes if economical.',
+        'Kids love staying in hotel rooms. They don\'t care about the size of the room and just want to jump on the beds! Also, they love watching TV, it doesn\'t really matter the language. A bunk bed can be a major fun machine!',
+        'Girls love eating hotel breakfast. So we try our best to find hotels that include breakfast. They pick their breakfast and eat really well.',
+      ]
+    },
+    {
+      id: 'packing',
+      title: '3. Packing',
+      tips: [
+        'Travel light -- two budget airline approved cabin bags and a backpack if a trip is less than a week. This way we don\'t have to worry about missing bags or wait at the baggage belt. This saves us time and extra money charged by budget airlines for checked languages. When a trip is more than a week or two, we pack a checked luggage, a cabin bag, and a backpack.',
+        'Toys -- We normally don\'t pack toys or anything special for kids. They have been very happy to play with whatever they pick-up on the way. Stones and shells from a beach are examples.',
+        "We pack our bags based on weather forecast. Layering is the best way to go.",
+        'We always carry a travel-friendly water bottle for kids so that we can refill it when empty.',
+        'Clothing for grown-ups: 1-2 shorts/pants for a one-week or less trip and a lightweight thin top for each day. A lightweight overshirt for summer just in case it gets a bit cold or breezy. A light-weight sufficiently warm jacket and a light-weight thin sweater for cold weather.',
+        'Clothing for kids: We pack generously for the kids. but one lightweight outfit for each day. If we don\'t have space to pack one outfit for each day, we wash their clothes at hotels. A lightweight zip-up or button-up shirt for summer and a good sufficiently warm jacket for other seasons. We always pack 1-2 very warm thin sweater for each kid for winter. Denim pants/shorts are the best for travels because they can be worn a couple of times before washing.',
+        'We never pack extra shoes. Everyone wears weather-appropriate good walking shoes for the entire duration. We have used hairdryers a few times to dry out our shoes after walking in the rain.',
+        'Packing for rain: We pack two small umbrellas if rain is in the forecast. However, sometimes it can be more convenient to buy two inexpensive big umbrellas when in the destination instead of carrying umbrellas and packing rain gears. We pack rain gear, if needed, only when we carry a checked luggage or our bags have enough extra space. We all have very thin lightweight raincoats.',
+        'We pack some fever reducers, prescription drugs if any, toothbrushes with caps, toothpaste, soap for kids, body lotion for kids and grownups (we use two travel size jars to carry our body location), deodorants, a hairbrush, bandages, and a few other essentials based on the space we have in our bags.',
+        'In summer, we pack our swimming suits and a lightweight beach towel.',
+        'We always carry our iPad (you never know when a kid gets too cranky in an airplane), some gummy bears, and small power bank.'
+      ]
+    },
+    {
+      id: 'travel',
+      title: '4. How we survive on airplanes, buses, and trains with kids',
+      tips: [
+        'If the girls are tired, they will just sleep.',
+        'On airplanes, girls love to fasten their seat belts and go through the safety manual. They always have enough questions, of course!',
+        'We buy them airplane snacks or meals. They are not so expensive compared to airport food. The kids pick what they want to eat, get our approval, wait for the food cart, and enjoy their meals. This part at least keep them occupied for an hour. Nikini is now able to order her food all by herself. When they are full, they usually feel sleepy which is perfect.',
+        'We carry our iPad loaded with quite a few offline kids\' games just in case a kid gets too cranky on airplanes or trains.',
+        'Trains are the easiest. When one is upset, we take a walk from one end to the other. Love the luxury of having mobility! Also, connecting with other passengers is easy on trains and that oftentimes help calm down the kids.',
+        'Buses are the most challenging method of transport with kids. We usually don\'t go anywhere that takes more than 2-3 hours by bus. Long bus rides make the kids exhausted very quickly. Nikini doesn\'t like bumpy bus rides anyway; they make her nauseatic.'
+      ]
+    },
+    {
+      id: 'exploring',
+      title: '5. Exploring',
+      tips: [
+        'We make a wish list of the places that we really want to see and plan our days accordingly. We move things around based on the weather and etc.',
+        'We all love to explore new places on foot. It\'s so much fun and flexible. It can certainly be easy (and sometimes cannot avoid) to use public transport in big cities. For example, we used public transport a lot in Paris, Barcelona, Istanbul, and Vienna.',
+        'We hardly visit museums. It\'s really tough with the girls. They get bored quite quickly.',
+        'Mornings are the best time for city strolls with kids. Afternoons are great for parks, beaches, and city squares.',
+        'Toilets: We take the girls to toilet every 2-3 hours. Kids say they want to "go" when they can\'t hold it anymore and it\'s not so easy to find a public toilet right away in some European cities. We plan our lunch, dinner, and coffee times accordingly. Cafes are the best places to go in a hurry. We need to pay in Europe to use public toilets.',
+        'Packing for daily sightseeing: We always carry a bottle of water, an extra outfit for each girl and a lightweight travelers\' towel in a small backpack. All important documents such as passports stay in a postal bag.  In summer, we carry everyone\'s\' swimming suits with us at all times. In addition, we always keep Sayuri\'s allergy medicine with us. Other things can be purchased in case of an emergency.',
+        'We all love river cruises over city sightseeing buses. So we usually don\'t miss boats/ferries/cruises if available.',
+        'Cable cars and Ferris wheels are some good entertainments for the kids!',
+        'Carousels can be life savers!',
+        'We find a bubble man/woman whenever the girls are too bored. Street musicians are an another good option. But chasing pigeons come after bubble popping.',
+        'Nikini eats at least 3 gelatos/ice creams in a day (season doesn\'t really matter). We buy ice cream later in the day so that they keep moving and stay motivated.',
+        'A 2-minute piggyback ride here and there can make Nikini walk more than 15 km (about 10 miles)! - Not an exaggeration!',
+        'We let them feel important by asking girls to help us with finding places. Nikini is already good. She remembers landmarks so well and knows her bearings most of the time. Sayuri can lead us to our room when we are at the hotel entrance. They both love reading maps -- of course in their very own ways.',
+        'Staying in water in summertimes and briefly visiting at least a small outdoor playground each day are some very important activities which always make everyone happy.',
+        'Sayuri enjoys picking rocks and Nikini loves taking photos of flowers, churches and interesting patterns and buildings.',
+      ]
+    },
+    {
+      id: 'eating',
+      title: '6. Eating',
+      tips: [
+        'The girls love eating breakfast at the hotel and we make sure to accommodate it whenever we can. It also saves us time and sometimes money too.',
+        'We like eating light lunches when traveling, but it\'s all dependent on where we are at. The girls usually eat a good lunch. We hardly order food from kids\' menu for them. The girls always find something great to eat from the regular menu. In the worst case, any restaurant will have pasta, fried calamari or soups! When in a city like Rome, we just eat so much! My girls tried so many different things in Rome and loved pasta carbonara. Once Nikini said, "this city has the best food in the whole wide world." We remember Sayuri eating a regular portion of a pasta carbonara all by herself!',
+        'We usually have early dinner at a good restaurant when traveling. The kids get tired by around 5 pm or so and we let them rest while having dinner. Again, we all order from the regular menu. We ask for kid size portions for the children if they have that option. Appetizers are the best! We order a couple of yummy appetizers for the kids and shares our food too if they are interested in trying. We always order local beer or wine and the kids also get their juice in a wine glass or a regular glass! This makes them feel important and grown up!',
+        'TripAdvisor review stickers can be a good way of finding a nice restaurant to dine in because when kids are hungry we have to feed them before they get too cranky!',
+        'In some countries like Spain, people eat late lunch and very late dinner. However, most restaurants that we went in made early lunch and dinner for us -- of course we had limited options!',
+        'We eat in very good restaurants with good TripAdvisor ratings. However, we don\'t go to fine restaurants where kids are not welcome or there is a dress code to follow.',
+        'In some cities such as Paris, eating is a formal activity and some say most restaurants don\'t welcome kids. When we were in Paris, we followed our usual method -- checked the menu, went in and asked if they were ok with the kids. Not a single restaurant has ever turned us back!',
+        'Greece, Italy, and Spain are some of the countries that love kids! People love the presence of kids around. When in Rome, a waiter even changed the TV channel from a sport channel to a kids\' channel just because of our kids. Another cafe in Paris did make lemonade just for Nikini (They didn\'t have it on the menu).',
+        'Some countries even Germany can be tricky! Some people might not tolerate the presence of kids. ha ha!  Our kids are loud and unpredictable. So when in doubt, we pick a Chinese or a Japanese restaurant. The girls love dim sum and sushi! Street food or takeouts can be a great option too!',
+        'We let kids eat sweets more than their usual when we are on the go. They are allowed to try different desserts and have a few ice creams.',
+        'We love European coffee shops! They have some best coffees and cakes.',
+        'We all love street food! Street food can be also a good way to get a good understanding of the culture and a good alternative when in a very expensive city like Zurich, Oslo or Stockholm.',
+        'Nikini loves eating fresh fruit from street vendors. Yes, it\'s impossible to wash them before eat so we only pick the best/cleanest!',
+        'Nikini has recently started ordering traditional food when in a new country. I am so proud of my little girl.',
+      ]
+    },
+    {
+      id: 'entertain',
+      title: '7. Keep the Kids entertained',
+      tips: [
+        'We don\'t plan our trips around the kids but try our best to keep them entertained. We plan our days to include a few entertainment items for the children.',
+        'The girls love walking. So we usually stroll along car-free streets or popular tourist streets.',
+        'Any European city will have an old town full of activities. Popping bubbles, chasing pigeons and dancing to the tunes of street musicians are great entertainments for them.',
+        'A ride on a Ferris wheel, carousel, or a cable car is enough to keep them excited at least a half day.',
+        'A beach day, fountains, swimming pools, water parks are great. The girls still remember the show at the magic fountain in Barcelona and having a dip at one of the fountains in Copenhagen.',
+        'Most of the European cities have good playgrounds. If we see one, we stop and let them play at least for 30 minutes.',
+        'Buying candies',
+        'Playing in parks (running, rolling, and etc)',
+        'Visiting zoos and aquariums (though rarely)',
+        'Taking photos. Nikini used to take photos (really good ones too) using my phone in 2013 and early 2014. So we bought a small camera for her in early-2014. She really enjoys taking photos of flowers, patterns, and churches. Sayuri also recently started asking for my phone to take photos',
+        'The girls love steps!',
+        'Learning a few foreign words, and Spotting flags.',
+        'NOTE: At the end of a trip, when we ask what part was the best, they usually quickly recall the 10 minutes they spent in a park or at a fountain or beach. That really tells you the trick!'
+      ]
+    },
+    {
+      id: 'sick',
+      title: '8. Sick Kids',
+      tips: [
+        'It\'s true -- kids sometimes get sick when you are on vacation. Our girls got sick several times when we were on vacation. Few times we had to seek medical attention.',
+        'Rome, Italy -- Nikini had started vomiting in the airplane from Berlin to Rome and had a high fever by the time we reached the hotel. We talked to the front desk and they gave us a couple of options and recommended calling a doctor to the hotel room. We went with their recommendation and  the hotel staff  contacted a doctor. A doctor came to our hotel room in less than 30 minutes. He examined Nikini, prescribed medication and spent almost an hours at the room. The total he charged for his service was 180Euro. Prescription medication cost us about 20 Euro.',
+        'Istanbul, Turkey -- It was very hot when we visited Istanbul. One day around noon Nikini developed a very high fever. Her symptoms suggested something like a heat stroke. We were very worried. That was a Sunday and nearby public hospitals and urgent care facilities were all closed. A local gentleman helped us finding information of a private hospital in the city. He also called a taxi and gave him all the instructions. The private hospital we went in was a fantastic facility. They registered us in their system quickly (there was no line) and assigned an English speaking gentleman to help us with the next step. Nikini had been seen by a doctor in less than 5 minutes from the registration. She had only a real bad sore throat. I was relieved. We got the total hospital bill for 100 Euro!!! Unbelievably inexpensive. Prescription drugs cost us only 10 Euro.',
+        'York, UK -- Sayuri was sick and had fever more than 2 days. We took her to the emergency room (it was a Sunday). We had to wait about 2-3 hours to see a doctor. She was having an ear infection. After seeing the doctor, I went to the front desk and inquired how to pay for the service. We usually pay first and reimburse from our insurance. The staff informed me that they don\'t have a methodology to charge me for the service and just wished us good luck and a good vacation!',
+        'Chicago, USA -- Sayuri had a very bad allergy reaction when visited a family with a cat. We immediately took her to a medical facility across the street. She received immediate medical care. The doctor charged $120 for her service and prescription medicine cost us $200. The doctor also prescribed an EpiPen which cost us $600 -- I think that\'s ridiculously expensive.',
+      ]
+    }
+  ]
+}
+
+export default data;
--- src/routes/Tips/index.js
+++ src/routes/Tips/index.js
@@ -1,13 +1,86 @@
 import React, { Component } from 'react';
+import { Container, Row, Col, ListGroup, ListGroupItem, Collapse } from 'reactstrap';
+import data from './data';
 // import './styles.css';
 
 class Trips extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = { 
+      // transBooking: false,
+      // hotelBooking: false,
+      packing: false,
+      travel: false,
+      exploring: false,
+      eating: false,
+      entertained: false,
+      sick: false
+    };
+  }
+
+  toggle(tip, event) {
+    event.preventDefault();
+    this.setState({
+      [tip]: !this.state[tip]
+    });
+  }
+
   render() {
     return (
-      <div className="container">
-        <h1>TIPS</h1>
-        <h2>Under construction...</h2>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <h1 className="display-4">Travel Tips</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p className="lead">
+              We used to travel a lot before the kids. We have seen most of Western Canada and parts of the USA and never thought it would be possible to do such travels with kids. Our travels in North America with kids was quite different from our travels in Europe with kids. In North America, we often drove and carried a car full of stuff even for a couple of days. Other times we flew to a major city, took a rental car, and drove to our attractions. We had to revisit our travel strategies when we temporary moved to Dresden. We don't own a car in Dresden because the city has an excellent public transport network and also we live in the city close to several tram stops and a train station. We thought it would be fun and a nice change to use the excellent public transport system in Europe and we have been correct with our decision so far. If you check our travels from 2014 and 2015, you will see that we have visited so many new places already. Our goal is to see most part of Europe. This was possible because of some luxuries that we don't have in the USA.
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="jumbotron">
+            <ul>
+              <li>Europe, especially Germany has a generous vacation policy! Thirty (30) vacation days is something we rarely hear in the USA. We make the full use of the vacation days by planning trips around weekends and public holidays,</li>
+              <li>Budget travel is possible in Europe. We usually travel on a budget, but our travels are very comfortable.</li>
+              <li>Europeans travel so much. There are so many budget airlines which offer tickets at a very low price to many European destinations. For example, we flew to Rome from Berlin and back for 60 Euros! </li>
+              <li>Europe has a well connected excellent train network. We use trains whenever it's convenient. Kids under 6 years usually travel free of charge. There are kids' compartments in some fast trains and regional trains.</li>
+            </ul>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p className="lead">The best travel tips for traveling with kids usually come from parents. Here are some tips for traveling with kids based on our family's travel experience.</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ListGroup>
+              {
+                data.topics.map((topic)=>{
+                  return (
+                    <ListGroupItem key={topic.id}>
+                      <a href="#" onClick={(e) => this.toggle(topic.id, e)}><h4>{topic.title}</h4></a>
+                      <Collapse isOpen={this.state[topic.id]}>
+                        <ul>
+                          {
+                            topic.tips.map((tip, i) => <li key={i}>{tip}</li>)
+                          }
+                        </ul>
+                      </Collapse>
+                    </ListGroupItem>
+                  )
+                })
+              }
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
