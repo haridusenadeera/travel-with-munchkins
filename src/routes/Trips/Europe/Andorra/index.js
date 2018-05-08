0e20bc98f4dfc5da100d6de06ee0e46feee05d6a
Haridu <haridu.senadeera@gmail.com>
2018-05-08 16:28:54 -0500

    moved carousel to the right of the row

--- src/routes/Trips/Europe/Andorra/index.js
+++ src/routes/Trips/Europe/Andorra/index.js
@@ -58,12 +58,12 @@ class Andorra extends Component {
         </Row>
         <Row>
           <Col xs="12" lg="6">
-            <Carousel items={items}/>
-          </Col>
-          <Col xs="12" lg="6">
             <p className="lead">
               We visited Andorra from Barcelona by bus. It's a very clean small city. This tiny nation is known for its ski resorts and duty-free shopping. We just did some sightseeing in the city. We visited this place because Hiroshan really wanted to have a quick peek at Andorra while in Barcelona. I think it's a beautiful place for hiking and winter sports rather than city sightseeing.
             </p>
+          </Col>
+          <Col xs="12" lg="6">
+            <Carousel items={items}/>
           </Col>
         </Row>
       </div>
