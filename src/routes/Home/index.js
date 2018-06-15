a8187b51acc2fd32023e663175774490764259e6
Haridu <haridu.senadeera@gmail.com>
2018-06-15 17:19:27 -0500

    modified the front page

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -63,6 +63,17 @@ class Home extends Component {
     return (
       <Container>
         <Row>
+          <Col className="text-center">
+            <h1 className="display-4">International travel with children</h1>
+            <hr />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="text-center">
+            <p className="lead">Click on the map to see our travels!</p>
+          </Col>
+        </Row>
+        <Row>
           <Col md={{size:6, offset:3}}>
             <WorldMap parentCallback={this.continentClicked} />
             { this.renderPopover() }
@@ -71,7 +82,6 @@ class Home extends Component {
         <Row>
           <Col>
             <div className="jumbotron">
-              <h1 className="display-4">International travel with children</h1>
               <p className="lead">
                 Thanks for visiting Travelwithmunchkins! My husband and I think that traveling with children is a must and decided to share our experience with other families who like to explore the world with kids. You might find some best travel tips that you need for international travel with children on a budget.
               </p>
@@ -83,17 +93,17 @@ class Home extends Component {
             <img className="img-fluid rounded" src={family} alt="family" />
           </Col>
           <Col md="8">
-            <p>
+            <p className="lead">
               When we started writing our family travel journal we realized that we have learned so much about comfortably traveling with kids. Usually, we don't visit museums, zoos, fun parks and aquariums. However, those are recommended by many travelers for kids. Our trips are usually short and we have only a few days to explore. So, we skip places such as zoos and museums and find alternatives to keep them entertained.
             </p>
           </Col>
         </Row>
         <Row>
           <Col md="9">
-            <p>
+            <p className="lead">
               Our travels are a bit unique because we don't plan our trips around kids and yet try our best to keep them entertained. So, we thought our travel experience might be helpful for parents like us who want to explore the world with young kids.
             </p>
-            <p>
+            <p className="lead">
               We temporary moved to Germany from the USA in early 2014 and decided to use this opportunity to explore Europe as much as we can. Most of the trips you see in our diary are within Europe. However, you will also see one or two trips to other continents. 
             </p>
           </Col>
@@ -116,10 +126,10 @@ class Home extends Component {
         </Row>
         <Row>
           <Col md="6">
-            <p>
+            <p className="lead">
               It's certainly challenging to travel with kids especially when they are very young. However, they get used to it when you start early.  It's fun to travel with kids. You get to see their exploding knowledge about the world. 
             </p>
-            <p>
+            <p className="lead">
               We don't use a professional camera. Our photos are either taken with a smartphone or Nikini's kids' friendly small camera.
             </p>
             <blockquote className="blockquote">
